3dbdc2a252201a75a55b276217bd806b499061a5
CraftyNavya <missnavya3111@gmail.com>
2024-04-14 21:46:56 +0530

    high

---
+++ server/webpack.config.js
@@ -0,0 +1,1 @@
+const path = require('path'); module.exports = { mode: 'production', entry: './server.js', output: { path: path.join(__dirname, 'dist'), publicPath: '/', filename: 'final.js', }, target: 'node',};
